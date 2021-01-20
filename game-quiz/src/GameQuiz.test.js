31a9ce0cdaf6df6519b79dd7503aa610f46eab0d
gwaqaza <philanimahele@gmail.com>
2021-01-20 19:58:50 +0200

    test: no answer, no background color.

--- game-quiz/src/GameQuiz.test.js
+++ game-quiz/src/GameQuiz.test.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GameQuiz from './GameQuiz';
+import Enzyme, { mount, shallow, render } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
+
+const state = {
+  turnData: {
+    books: ['The Shining', 'IT', 'David Copperfield', 'A Tale of Two Cities', 'Macbeth'],
+    author: {
+      name: 'Charles Dickens',
+      imageUrl: 'images/author/charlesdickens.jpg',
+      imageSource: 'Wikimedia Commons',
+      books: ['David Copperfield', 'A Tale of Two Cities']
+    },
+  },
+  highlight: 'none'
+}
 
 describe("Game Quiz", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div");
-    ReactDOM.render(<GameQuiz />, div);
+    ReactDOM.render(<GameQuiz {...state} onAnswerSelected={() => {}}/>, div);
   });
+
+  describe("When no answer has been selected", () => {
+    let wrapper;
+    beforeAll(() => {
+      wrapper = mount(<GameQuiz {...state} onAnswerSelected={() => {}}/>, div)
+    });
+
+    it("should not have a background color", () => {
+      expect(wrapper.find("div.row.turn").props().style.backgroundColor).toBe('');
+    });
+  });
+
 });
